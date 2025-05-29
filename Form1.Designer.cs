ff0b73548bf8cba7c5405754881f9581ab10089c
Montanha <azt1337@hotmail.com>
2025-05-29 09:02:37 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Juntador_de_Atestados
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+	#region Windows Form Designer generated code
+
+	/// <summary>
+	///  Required method for Designer support - do not modify
+	///  the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{
+	 tb_atestados = new TextBox();
+	 tb_resultado = new TextBox();
+	 button1 = new Button();
+	 button2 = new Button();
+	 SuspendLayout();
+	 // 
+	 // tb_atestados
+	 // 
+	 tb_atestados.Dock = DockStyle.Fill;
+	 tb_atestados.Location = new Point(0, 0);
+	 tb_atestados.Multiline = true;
+	 tb_atestados.Name = "tb_atestados";
+	 tb_atestados.Size = new Size(340, 174);
+	 tb_atestados.TabIndex = 0;
+	 tb_atestados.TextChanged += tb_atestados_TextChanged;
+	 // 
+	 // tb_resultado
+	 // 
+	 tb_resultado.Location = new Point(0, 151);
+	 tb_resultado.Name = "tb_resultado";
+	 tb_resultado.Size = new Size(251, 23);
+	 tb_resultado.TabIndex = 1;
+	 // 
+	 // button1
+	 // 
+	 button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+	 button1.Location = new Point(315, 151);
+	 button1.Name = "button1";
+	 button1.Size = new Size(25, 23);
+	 button1.TabIndex = 2;
+	 button1.Text = "?";
+	 button1.UseVisualStyleBackColor = true;
+	 button1.Click += button1_Click;
+	 // 
+	 // button2
+	 // 
+	 button2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+	 button2.Location = new Point(257, 151);
+	 button2.Name = "button2";
+	 button2.Size = new Size(59, 23);
+	 button2.TabIndex = 3;
+	 button2.Text = "Novo";
+	 button2.UseVisualStyleBackColor = true;
+	 button2.Click += button2_Click;
+	 // 
+	 // Form1
+	 // 
+	 AutoScaleDimensions = new SizeF(7F, 15F);
+	 AutoScaleMode = AutoScaleMode.Font;
+	 ClientSize = new Size(340, 174);
+	 Controls.Add(button2);
+	 Controls.Add(button1);
+	 Controls.Add(tb_resultado);
+	 Controls.Add(tb_atestados);
+	 Name = "Form1";
+	 Text = "Juntatest 1.1";
+	 ResumeLayout(false);
+	 PerformLayout();
+	}
+
+	#endregion
+
+	private TextBox tb_atestados;
+	private TextBox tb_resultado;
+	private Button button1;
+	private Button button2;
+ }
+}
---
+++ Form1.cs
@@ -0,0 +1,281 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace Juntador_de_Atestados
+{
+
+
+ // ----------------------------------------
+ // FORM PRINCIPAL
+ // ----------------------------------------
+ public partial class Form1 : Form
+ {
+	public Form1()
+	{
+	 InitializeComponent(); // inicializa os controles
+	 this.Load += Form1_Load; // associa evento de load
+	}
+	// ----------------------------------------
+	// CLASSE ESTADO_JANELA
+	// Armazena posição da janela em JSON
+	// ----------------------------------------
+	class estado_janela
+	{
+	 public int left { get; set; }  // posição horizontal
+	 public int top { get; set; }   // posição vertical
+	}
+	// ----------------------------------------
+	// RESTAURA POSIÇÃO SALVA DA JANELA
+	// ----------------------------------------
+	private void Form1_Load(object sender, EventArgs e)
+	{
+	 try
+	 {
+		if (File.Exists("juntatest.cfg"))
+		{
+		 string json = File.ReadAllText("juntatest.cfg");
+		 var estado = JsonSerializer.Deserialize<estado_janela>(json);
+
+		 if (estado != null)
+		 {
+			this.Left = estado.left;
+			this.Top = estado.top;
+		 }
+		}
+	 }
+	 catch
+	 {
+		// ignora erro silenciosamente
+	 }
+	}
+
+	// ----------------------------------------
+	// EVENTO TEXTCHANGED - tb_atestados
+	// Calcula o maior período confluente a
+	// partir da entrada digitada em tb_atestados
+	// ----------------------------------------
+	private void tb_atestados_TextChanged(object sender, EventArgs e)
+	{
+	 tb_resultado.Text = ""; // limpa resultado anterior
+
+	 try
+	 {
+		string input = tb_atestados.Text.Trim(); // pega texto digitado
+		if (string.IsNullOrWhiteSpace(input)) return; // ignora vazio
+
+		string[] partes = input.Split(' '); // separa por espaço
+		List<(DateTime inicio, DateTime fim)> periodos = new List<(DateTime, DateTime)>(); // lista de períodos
+		DateTime agora = DateTime.Today; // data atual
+
+		// ----------------------------------------
+		// MODO ESPECIAL: APENAS DUAS DATAS
+		// ----------------------------------------
+		if (partes.Length == 2)
+		{
+		 DateTime data1 = parse_data(partes[0], agora); // converte primeira data
+		 DateTime data2 = parse_data(partes[1], agora); // converte segunda data
+
+		 if (data2 < data1) throw new Exception(); // não pode ter fim antes do início
+
+		 int dias = (data2 - data1).Days + 1; // calcula número de dias corridos
+		 DateTime fim = data1.AddDays(dias - 1); // calcula fim real (seguro)
+
+		 periodos.Add((data1, fim)); // adiciona como período
+		}
+		else
+		{
+		 // ----------------------------------------
+		 // MODO NORMAL: data dias data dias ...
+		 // ----------------------------------------
+		 if (partes.Length < 2 || partes.Length % 2 != 0)
+			throw new Exception(); // precisa de pares
+
+		 for (int i = 0; i < partes.Length; i += 2)
+		 {
+			string strdata = partes[i];      // parte da data
+			string strdias = partes[i + 1];  // parte dos dias
+
+			int dias = int.Parse(strdias); // converte dias
+			DateTime data = parse_data(strdata, agora); // converte data
+
+			DateTime fim = data.AddDays(dias - 1); // calcula fim do período
+			periodos.Add((data, fim)); // adiciona o par
+		 }
+		}
+
+		// ----------------------------------------
+		// AGRUPA PERÍODOS CONFLUENTES
+		// ----------------------------------------
+		periodos = periodos.OrderBy(p => p.inicio).ToList();
+		List<(DateTime ini, DateTime fim)> blocos = new List<(DateTime, DateTime)>();
+
+		DateTime atual_ini = periodos[0].inicio;
+		DateTime atual_fim = periodos[0].fim;
+
+		for (int i = 1; i < periodos.Count; i++)
+		{
+		 if (periodos[i].inicio <= atual_fim.AddDays(1))
+		 {
+			atual_fim = (periodos[i].fim > atual_fim) ? periodos[i].fim : atual_fim;
+		 }
+		 else
+		 {
+			blocos.Add((atual_ini, atual_fim));
+			atual_ini = periodos[i].inicio;
+			atual_fim = periodos[i].fim;
+		 }
+		}
+		blocos.Add((atual_ini, atual_fim));
+
+		// ----------------------------------------
+		// PEGA O MAIOR BLOCO
+		// ----------------------------------------
+		var maior = blocos.OrderByDescending(b => (b.fim - b.ini).TotalDays).First();
+		int duracao = (maior.fim - maior.ini).Days + 1;
+
+		tb_resultado.Text = $"Maior prazo: {maior.ini:dd/MM/yy} - {duracao} dias (até {maior.fim:dd/MM/yy})";
+
+		finaliza(); // salva posição da janela
+	 }
+	 catch
+	 {
+		tb_resultado.Text = "Aperte o botão ? para ajuda.";
+	 }
+	}
+
+	// ----------------------------------------
+	// SALVA POSIÇÃO DA JANELA EM JSON
+	// ----------------------------------------
+	private void finaliza()
+	{
+	 try
+	 {
+		var estado = new estado_janela
+		{
+		 left = this.Left,
+		 top = this.Top
+		};
+
+		string json = JsonSerializer.Serialize(estado);
+		File.WriteAllText("juntatest.cfg", json); // salva no mesmo diretório
+	 }
+	 catch
+	 {
+		// ignora erro
+	 }
+	}
+
+	// ----------------------------------------
+	// FUNÇÃO DE PARSE DE DATA
+	// Interpreta dd/mm, dd/mm/yy, ddmmaaaa etc.
+	// ----------------------------------------
+	private DateTime parse_data(string strdata, DateTime agora)
+	{
+	 if (strdata.Contains("/")) // formato com barra
+	 {
+		string[] pedacos = strdata.Split('/');
+		int d = int.Parse(pedacos[0]);
+		int m = int.Parse(pedacos[1]);
+		int y = (pedacos.Length >= 3) ? int.Parse(pedacos[2]) : agora.Year;
+		if (y < 100) y += 2000;
+		DateTime data = new DateTime(y, m, d);
+		if (data > agora && pedacos.Length < 3) data = data.AddYears(-1);
+		return data;
+	 }
+	 else if (strdata.Length == 6 || strdata.Length == 8) // ddmmaa ou ddmmaaaa
+	 {
+		int d = int.Parse(strdata.Substring(0, 2));
+		int m = int.Parse(strdata.Substring(2, 2));
+		int y = int.Parse(strdata.Substring(4));
+		if (y < 100) y += 2000;
+		return new DateTime(y, m, d);
+	 }
+	 else
+	 {
+		throw new Exception(); // formato inválido
+	 }
+	}
+
+	// ----------------------------------------
+	// BOTÃO AJUDA "?" 
+	// Mostra informações do programa e instruções
+	// ----------------------------------------
+	private void button1_Click(object sender, EventArgs e)
+	{
+	 string msg =
+ @"Nome do programa: Juntatest 1.0. Livre distribuição.
+Autor: Ramiro Ayres Reggiani, perito médico federal.
+Contato: (42) 99923-8123 (WhatsApp)
+Licença: Freeware.
+GitHub: https://github.com/Zuabros/juntatest (GNU)
+
+----------------------------------------
+O QUE O PROGRAMA FAZ
+----------------------------------------
+Recebe uma sequência de atestados contendo data e número de dias de afastamento.
+
+Calcula automaticamente o maior período confluente de afastamento 
+(somando atestados que se sobrepõem ou se encostam),
+e informa a data de início, a duração total em dias corridos e a data final.
+
+----------------------------------------
+FORMATO DE ENTRADA (na caixa de cima)
+----------------------------------------
+Digite os atestados separados por espaço, no formato:
+
+data dias data dias ...
+
+Exemplo:
+10/01/25 5 20/01/25 10 05/02/25 15
+
+Também é possível digitar apenas duas datas. Nesse caso, 
+o programa considera que a primeira é o início e a segunda é o último dia do afastamento.
+
+Exemplo:
+10/01/25 15/01/25
+
+----------------------------------------
+BOTÃO ""NOVO""
+----------------------------------------
+Limpa os campos e copia para a área de transferência a data de início 
+do maior afastamento, no formato dd/mm/yyyy.";
+
+	 MessageBox.Show(msg, "Ajuda - Juntatest", MessageBoxButtons.OK, MessageBoxIcon.Information);
+	}
+
+	// ----------------------------------------
+	// BOTÃO NOVO
+	// Limpa campos e copia a data inicial do último resultado para a área de transferência
+	// ----------------------------------------
+	private void button2_Click(object sender, EventArgs e)
+	{
+	 try
+	 {
+		string txt = tb_resultado.Text;
+		if (txt.StartsWith("Maior prazo: "))
+		{
+		 // extrai a data inicial no formato dd/mm/yy
+		 string data_curtinha = txt.Substring("Maior prazo: ".Length, 8); // pega 8 caracteres da data
+
+		 // converte para datetime com ano completo
+		 DateTime data_ini = DateTime.ParseExact(data_curtinha, "dd/MM/yy", null);
+
+		 // copia como dd/MM/yyyy
+		 Clipboard.SetText(data_ini.ToString("dd/MM/yyyy"));
+		}
+	 }
+	 catch
+	 {
+		// ignora erro
+	 }
+
+	 // limpa os campos
+	 tb_atestados.Text = "";
+	 tb_resultado.Text = "";
+	}
+ }
+}
